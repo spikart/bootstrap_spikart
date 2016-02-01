6b7f7635df185a9ff28cdb6e2afa12c89a9f8c9e
spikart <zyuralex@gmail.com>
2016-02-01 19:24:26 +0200

    Changed style.css and index.php

--- index.php
+++ index.php
@@ -18,71 +18,49 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body>
-<div class="navbar navbar-inverse navbar-fixed-top">
+            <!--Верхнє меню-->
+<div class="navbar-fixed-top" id="top_home">
     <nav id="top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
-                    <span class="sr-only">Відкрити меню</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-          <!---<a class="navbar-brand" href="#">Logo</a>--->
+        <div class="container-fluid"><div class="nav pull-right" id="top-links">
+                <ul class="list-inline">
+                    <li><a href="#"><i class="fa fa-home"></i>
+                            Головна</a></li>
+                    <li><a href="http://spikart.com.ua/shop/"><i class="fa fa-shopping-bag"></i>Магазин</a></li>
+                    <li><a href="#"><i class="fa fa-info-circle"></i>Про нас</a></li>
+                    <li><a href="#"><i class="fa fa-arrows-alt"></i>Напрямки діяльності</a></li>
+                    <li><a href="#"><i class="fa fa-info-circle"></i>Контакти</a></li>
+                </ul>
             </div>
-                <div class="collapse navbar-collapse pull-right" id="responsive-menu">
-                    <ul class="nav navbar-nav">
-
-                        <li><a href="#""><i class="fa fa-home"></i>Головна</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-shopping-bag"></i>
-                                Магазин <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Гаджети</a> </li>
-                                <li><a href="#">Дітям</a></li>
-                                <li><a href="#">Комап'ютери</a></li>
-                                <li><a href="#">Мережа</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#"><i class="fa fa-info-circle"></i>Про нас</a></li>
-                        <li><a href="#"><i class="fa fa-arrows-alt"></i>
-                                Напрямки діяльності</a></li>
-                        <li>
-                            <a title="Контакти" href="#">
-                                <i class="fa fa-info-circle"></i>
-                                <span class="hidden-xs hidden-sm hidden-md">Контакти</span>
-
-                            </a></li>
-                    </ul>
-                </div>
         </div>
     </nav>
 </div>
-
-<div class="container-fluid " id="logo">
-    <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-            <img class="img-responsive" src="images/logo_center.png" alt="">
-        </div>
-        <div class="nav navbar-nav col-lg-8 col-md-8 col-sm-8 col-xs-8">
-           <ul class="social-icons">
-                <li><i href="#"><i class="fa fa-facebook"></i> </li>
-                <li><i href="#"><i class="fa fa-google-plus"></i> </li>
-                <li><i href="#"><i class="fa fa-vk"></i> </li>
-                <li><i href="#"><i class="fa fa-skype"></i> </li>
-           </ul>
+            <!--Шапка-->
+<header id="header-container">
+    <div id="header-inner-container">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-2">
+                    <div id="logo">
+                        <img src="images/logo_center.png" alt="Спікарт" title="Spikart" class="img-responsive">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-           <!--Індикатори слайдів-->
+
+</header>
+
+            <!--Індикатори слайдів-->
 <div id="carousel" class="carousel slide">
+    <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
     <ol class="carousel-indicators">
         <li class="active" data-target="#carousel" data-slide-to="0"></li>
         <li data-target="#carousel" data-slide-to="1"></li>
         <li data-target="#carousel" data-slide-to="2"></li>
     </ol>
-       <!--Слайди-->
+            <!--Слайди-->
     <div class="carousel-inner">
         <div class="item active">
             <img src="images/proba21.jpg" alt="">
@@ -106,13 +84,16 @@
             </div>
         </div>
     </div>
-       <!--Стрілки слайдів-->
+            <!--Стрілки слайдів-->
     <a href="#carousel" class="left carousel-control" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
     </a>
     <a href="#carousel" class="right carousel-control" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right"></span>
     </a>
+        </div>
+            <div class="col-sm-3"></div>
+        </row>
 </div>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
