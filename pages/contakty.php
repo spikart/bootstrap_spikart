0978b56b83c4ea1cf7459bbdf25bfb73dfd1d73d
spikart <zyuralex@gmail.com>
2016-02-19 21:32:23 +0200

    new page contakty.php

--- pages/contakty.php
+++ pages/contakty.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <link rel="shortcut icon" href="/images/favicon_mini.png" type="image/png">
+    <link rel="shortcut icon" href="../images/favicon_mini.png" type="image/png">
     <title>Spikart</title>
     <!-- Bootstrap -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/font-awesome.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/font-awesome.css" rel="stylesheet">
+    <link href="../css/animate.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans|Roboto+Slab' rel='stylesheet' type='text/css'>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -22,12 +22,9 @@
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.js"></script>
+    <script src="../js/bootstrap.js"></script>
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-    <script src="js/jquery.jrumble.1.3.min.js"></script>
-
-
-
+    <script src="../js/jquery.jrumble.1.3.min.js"></script>
     <![endif]-->
 </head>
 <body>
@@ -73,9 +70,9 @@
                 <div class="col-sm-1"></div>
                 <div class="col-sm-3">
                     <ul class="social">
-                        <li><a href="#" target="_blank"><img src="images/social/vk_logo_200.png" alt="Вконтаке" title="Вконтаке" class="img-responsive"></a></li>
-                        <li><a href="#" target="_blank"><img src="images/social/google-plus_logo_200.png" alt="Google+" title="Google+" class="img-responsive"></a></li>
-                        <li><a href="#" target="_blank"><img src="images/social/facebook_logo_200.png" alt="Facebook" title="Facebook" class="img-responsive"></a></li>
+                        <li><a href="#" target="_blank"><img src="../images/social/vk_logo_200.png" alt="Вконтаке" title="Вконтаке" class="img-responsive"></a></li>
+                        <li><a href="#" target="_blank"><img src="../images/social/google-plus_logo_200.png" alt="Google+" title="Google+" class="img-responsive"></a></li>
+                        <li><a href="#" target="_blank"><img src="../images/social/facebook_logo_200.png" alt="Facebook" title="Facebook" class="img-responsive"></a></li>
                     </ul>
                 </div>
                 <div class="col-sm-8">
@@ -92,10 +89,10 @@
 
                             <li><a href="http://www.bs-spikart.com.ua/index.php""><i class="fa fa-home"></i>Головна</a></li>
 
-                            <li><a href="#title-1"><i class="fa fa-arrows-alt"></i>
+                            <li><a href="../index.php#title-1"><i class="fa fa-arrows-alt"></i>
                                     Напрямки діяльності</a></li>
                             <li>
-                                <a href="pages/contakty.php"><i class="fa fa-info-circle"></i>Контакти</a></li>
+                                <a href="#"><i class="fa fa-info-circle"></i>Контакти</a></li>
                         </ul>
                     </div>
                 </div>
@@ -116,8 +113,8 @@
             <div class="row">
                 <div class="col-sm-2">
                     <div class="logo">
-                        <a href="index.php">
-                        <img src="images/logo_center.png" alt="Спікарт" title="Spikart" class="img-responsive">
+                        <a href="../index.php">
+                        <img src="../images/logo_center.png" alt="Спікарт" title="Spikart" class="img-responsive">
                         </a>
                     </div>
                 </div>
@@ -147,8 +144,8 @@
                         <div class="col-sm-5"></div>
                         <div class="col-sm-3">
                             <ul class="skype-mail">
-                                <li><a href="#" target="_blank"><img src="images/social/skype_logo_200.png" id="skype" alt="Skype" title="Skype" class="img-responsive"></a></li>
-                                <li><a href="#" target="_blank"><img src="images/social/e-mail_logo_200.png" id="email" alt="E-mail" title="E-mail" class="img-responsive"></a></li>
+                                <li><a href="#" target="_blank"><img src="../images/social/skype_logo_200.png" id="skype" alt="Skype" title="Skype" class="img-responsive"></a></li>
+                                <li><a href="#" target="_blank"><img src="../images/social/e-mail_logo_200.png" id="email" alt="E-mail" title="E-mail" class="img-responsive"></a></li>
                             </ul>
                         </div>
 
@@ -182,7 +179,7 @@
 
                         <div class="col-sm-4">
                             <div id="ukrtelecom">
-                                <li><a href="#" target="_blank"><img src="images/phone/ukrtelecom.png"
+                                <li><a href="#" target="_blank"><img src="../images/phone/ukrtelecom.png"
                                                                      alt="+38(032) 276 15 20" title="+38(032) 276 15 20"
                                                                      class="img-responsive"></a></li></div>
                             <div id="numb-phone">
@@ -193,7 +190,7 @@
                         <div class="col-sm-4">
                             <div id="kyivstar">
 
-                                <li><a href="#" target="_blank"><img src="images/phone/kyivstar.png"
+                                <li><a href="#" target="_blank"><img src="../images/phone/kyivstar.png"
                                                                      alt="+38(096) 091 22 11" title="+38(096) 091 22 11"
                                                                      class="img-responsive"></a></li></div>
                                 <div id="numb-phone">
@@ -205,7 +202,7 @@
                         <div class="col-sm-4">
                             <div id="lifecell">
 
-                                <li><a href="#" target="_blank"><img src="images/phone/lifecell.png"
+                                <li><a href="#" target="_blank"><img src="../images/phone/lifecell.png"
                                                                      alt="+38(073) 073 22 11" title="+38(073) 073 22 11"
                                                                      class="img-responsive"></a></li></div>
                             <div id="numb-phone">
@@ -220,121 +217,76 @@
     </div>
 </div>
 
-<!--Слайдшоу-->
-<!--Індикатори слайдів-->
-<div class="carousel slide" id="carousel" data-interval="2500">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-3 col-md-3 col-lg-3"></div>
-            <div class="col-sm-6 col-md-6 col-lg-6">
-                <ol class="carousel-indicators">
-                    <li class="active" data-target="#carousel" data-slide-to="0"></li>
-                    <li data-target="#carousel" data-slide-to="1"></li>
-                    <li data-target="#carousel" data-slide-to="2"></li>
-                </ol>
-                <!--Слайди-->
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img src="images/proba21.png" alt="">
-                        <div class="carousel-caption">
-                            <h3>Перший слайд</h3>
-                            <p>Перегляд слайду!!!</p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="images/proba22.png" alt="">
-                        <div class="carousel-caption">
-                            <h3>Другий слайд</h3>
-                            <p>Перегляд слайду!!!</p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="images/proba24.png" alt="">
-                        <div class="carousel-caption">
-                            <h3>Третій слайд</h3>
-                            <p>Перегляд слайду!!!</p>
-                        </div>
-                    </div>
-                </div>
-                <!--Стрілки слайдів-->
-                <a href="#carousel" class="left carousel-control" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                </a>
-                <a href="#carousel" class="right carousel-control" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
+<!--Контaкти-->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-5"></div>
+        <div class="col-sm-2" id="contakty"><h2>КОНТАКТИ</h2></div>
+        <div class="col-sm-5"><br><br><br><br><br></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-4" id="maneger">
+            <h3>Менеджер</h3>
+            <div class="row">
+                <div class="col-sm-6">
+                    <img src="../images/contacts/maneger.png" class="img-responsive"></div>
+                <div class="col-sm-6"><ul>
+                        <li><h4>Трофуша Тарас</h4></li>
+                        <li>тел. +38(097)979-63-63</li>
+                        <li>тел. +38(093)47-88-944</li>
+                        <li>ICQ 244-934-340</li>
+                        <li>Skype: Trof</li>
+                    </ul></div>
+
             </div>
-            <div class="col-sm-3 col-md-3 col-lg-3"></div>
         </div>
-    </div>
-</div>
-<!--Тіло-->
-<div>
-    <div class="container-fluid" id="title-1">
-        <p>НАПРЯМКИ ДІЯЛЬНОСТІ</p>
-    </div>
-    <div class="container-fluid" id="special-1">
-        <div class="row">
-            <div class="col-md-4"><img src="images/1automatization.png" alt="АВТОМАТИЗАЦІЯ БІЗНЕСУ"
-                                       title="АВТОМАТИЗАЦІЯ БІЗНЕСУ">
-                <p>АВТОМАТИЗАЦІЯ БІЗНЕСУ</p></div>
-            <div class="col-md-4"><img src="images/1hardware.png" alt="КОМП'ЮТЕРИ ТА ОРГТЕХНІКА"
-                                       title="КОМП'ЮТЕРИ ТА ОРГТЕХНІКА">
-                <p>КОМП'ЮТЕРИ ТА ОРГТЕХНІКА</p></div>
-            <div class="col-md-4"><img src="images/1remontmacbook.png" alt="РЕМОНТ НОУТБУКІВ" title="РЕМОНТ НОУТБУКІВ">
-                <p>РЕМОНТ НОУТБУКІВ</p></div>
+        <div class="col-sm-2"></div>
+        <div class="col-sm-4" id="accounting">
+            <h3>Бухгалтерія</h3>
+            <div class="row">
+                <div class="col-sm-6"><img src="../images/contacts/accounting.png" class="img-responsive"></div>
+                <div class="col-sm-6"><ul>
+                        <li><h4>Котецька Олександра</h4></li>
+
+                        <li>+38(093)87-98-497</li>
+                        <li>ICQ 581-326-754</li>
+                        <br><br>
+
+                    </ul>
+                </div>
+
+            </div>
         </div>
-        <div class="row">
-            <div class="col-md-4"><img src="images/1computersale.png" alt="РЕМОНТ КОМП'ЮТЕРІВ"
-                                       title="РЕМОНТ КОМП'ЮТЕРІВ">
-                <p>РЕМОНТ КОМП'ЮТЕРІВ</p></div>
-            <div class="col-md-4"><img src="images/1hifinew1.png" alt="HiFi АУДІОСИСТЕМИ" title="HiFi АУДІОСИСТЕМИ">
-                <p>HiFi АУДІОСИСТЕМИ</p></div>
-            <div class="col-md-4"><img src="images/1toner.png" alt="ЗАПРАВКА КАТРИДЖІВ" title="ЗАПРАВКА КАТРИДЖІВ">
-                <p>ЗАПРАВКА КАТРИДЖІВ</p></div>
-        </div>
+        <div class="col-sm-1"></div>
     </div>
-</div>
-<div id="about-as">
-    <img src="images/logo_center.png" alt="Спікарт" title="Spikart"">
-</div>
-<div id="title-2">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-1"></div>
-            <div class="col-md-3" id="we-center"><i class="fa fa-paper-plane-o fa-4x"></i><br>МИ РЕАЛІЗОВУЄМО<br>
-                <div id="we">
-                    <br>комп'ютерну техніку
-                    <br>портативну техніку
-                    <br>мобільні пристрої
-                    <br>побутову техніку оргтехніку
+    <div class="row" id="map-adress">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-5" id="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d643.3553283821451!2d24.03598209199599!3d49.83454069613703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x5d05cf85e010ad13!2z0J_QnyAi0KHQv9GW0LrQsNGA0YIi!5e0!3m2!1suk!2sua!4v1455890375587" width="500" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <div class="col-sm-1" id="col1"></div>
+
+            <div class="col-sm-4" id="form">
+                <h3>Адреса</h3>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <img src="../images/contacts/home.png" class="img-responsive"></div>
+                    <div class="col-sm-6"><ul>
+                            <li>79005 м.Львів.</li>
+                            <li>Вул. Зелена 5д/5.</li>
+                            <li>Тел./Факс +38(032)276-15-20</li>
+                            <li>Київстар  +38(096)091-22-11</li>
+                            <li>Lifecell  +38(073)073-22-11</li>
+                            <li>e-mail: store@spikart.com.ua</li>
+                        </ul>
+                    </div>
+
                 </div>
             </div>
-            <div class="col-md-4"><i class="fa fa-university fa-4x"></i><br>ПРО НАС
-                <div id="we-top">
-                    <br>Приватне підприємство "Спікарт"
-                    <br>засноване 7 жовтня 2003 року.
-                    <br>З часу заснування ми займаємося
-                    <br>всіма спектрами товарів і послуг,
-                    <br>які можна надавати в сфері інформатизації
-                    <br>як підприємництва так і приватного сектору.
-                    <br> Обирайте нашу компанію і
-                    <br>впевненість у Вашій покупці та
-                    <br>тривалий час безвідказної роботи
-                    <br>Вашого вибору ми Вам гаратнуємо.
-                </div>
-            </div>
-            <div class="col-md-3"><i class="fa fa-star fa-4x"></i><br>МИ ПРОПОНУЄМО ВАМ
-                <br>
-                <div id="we">
-                    <h4>найякіснішу продукцію</h4>
-                    <h4>готові рішення під ключ</h4>
-                    <h4>компромісні рішення</h4>
-                    <h4>модернізацію існуючого парку техніки</h4></div>
-            </div>
-        </div>
+
+        <div class="col-sm-1"></div>
     </div>
-    <div class="col-md-1"></div>
 </div>
 <!--Футтер-->
 <div id="footer">
@@ -347,13 +299,14 @@
     </div>
     <div id="adress-footer">
 
-    <div id="adress-top">
-        79005 м. Львів. Вул. Зелена 5д/5.
-        <br>Тел./Факс +3(032)276-15-20
-        <br>e-mail: <a href="#">store@spikart.com.ua</a>
+        <div id="adress-top">
+            79005 м. Львів. Вул. Зелена 5д/5.
+            <br>Тел./Факс +3(032)276-15-20
+            <br>e-mail: <a href="#">store@spikart.com.ua</a>
         </div>
     </div>
 </div>
+
 <!--
 <div class="container" id="top-footer">
     <div class="container-fluid">
